e71cbd500b722df2ba2a4e00d0a5447d5db5161a
shreya8776 <shreya.c.pandey@capgemini.com>
2025-03-11 10:08:25 +0530

    update requirements

--- api.py
+++ api.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_api():
-    return jsonify({'message': 'Hello, World!'})
+    return jsonify({'message': 'Hello, World of Api!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
