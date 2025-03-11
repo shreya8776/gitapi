3e8934a24c8e50f3df94d972764319f4b7f1cf9d
shreya8776 <shreya.c.pandey@capgemini.com>
2025-03-11 10:26:44 +0530

    Flask requirements

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
 
 from flask import Flask, jsonify
-
+from asgiref.wsgi import WsgiToAsgi
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_api():
-    return jsonify({'message': 'Hello, World of Api!'})
+    return jsonify({'message': 'Hello, World FlaskApi!'})
+
+asgi_app = WsgiToAsgi(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
